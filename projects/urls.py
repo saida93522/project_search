77ab4015e7c6b8566a917e65c15565ec66586615
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-12-24 13:43:35 -0600

    feat: add single-project path

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', views.projects, name='projects'),
-    path('single_project/', views.single_project, name='single_project')
+    path('project/<str:pk>/', views.single_project, name='project')
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,12 +2,37 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
 
 def home(request):
     return render(request,'home.html')
 
 def projects(request):
-    return render(request,'projects/projects.html')
+    msg = 'projects'
+    number = 10
+    context = {'msg':msg, 'num':number, 'projects':projectsList}
+    return render(request,'projects/projects.html', context)
 
-def single_project(request):
-    return render(request,'projects/single_project.html')
+def single_project(request,pk):
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    context = {'project':projectObj}
+    return render(request,'projects/single_project.html',context)
