c9ea39d8c4ba1e7b174df6d08f9c5bd56e81289d
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-12-28 16:34:06 -0600

    feat: add,edit,delete project

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', views.projects, name='projects'),
     path('project/<str:pk>/', views.single_project, name='project'),
-    path('create-project/', views.create_project, name='create-project')
+    path('create-project/', views.create_project, name='create-project'),
+    path('edit-project/<str:pk>/', views.edit_project, name='edit-project'),
+    path('delete-project/<str:pk>/', views.delete_project, name='delete-project')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from  .models import Project, Review, Tag
 # Create your views here.
 from django.shortcuts import render
@@ -22,5 +22,32 @@ def single_project(request,pk):
 
 def create_project(request):
     form = ProjectForm() #extensiate from projectForm
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    
     context = {'form':form}
     return render(request,'projects/project_form.html', context )
+
+
+def edit_project(request,pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project) 
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    
+    context = {'form':form}
+    return render(request,'projects/project_form.html', context )
+
+def delete_project(request,pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object':project}
+    return render(request,'projects/delete_template.html', context )
