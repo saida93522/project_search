239b391059f299360e1c05cd82f29eaa9e285a62
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-12-30 13:52:38 -0600

    feat: add 'input' attribute in ProjectForm

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project, Review, Tag
 
 
@@ -6,3 +7,14 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title','feature_image','description', 'demo_link','source_link','tags']
+
+        widgets ={
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+    def __init__(self, *args, **kwargs):
+        """ override init and modify/update project form fields. """
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+        # self.fields['title'].widget.attrs.update({'class':'input'})
+        # self.fields['description'].widget.attrs.update({'class':'input'})
