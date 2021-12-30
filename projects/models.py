330b7787a02215b85d01d868834f284735d2dd8b
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-12-30 15:33:04 -0600

    feat: allow project app to extend profile

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 import uuid
-
+from users.models import Profile
 class Project(models.Model):
+    owner = models.ForeignKey(Profile,null=True,blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     feature_image = models.ImageField(null=True, blank=True,default='default.jpg')
--- users/views.py
+++ users/views.py
@@ -4,5 +4,6 @@ from django.contrib import messages
 from .models import User,Profile
 
 def profiles(request):
-    context = {}
+    users = Profile.objects.all()
+    context = {'users':users}
     return render(request, 'users/profiles.html',context)
