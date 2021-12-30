1387cab141bf97dda8f508e0370076a0771d21cc
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-12-30 14:14:41 -0600

    feat: create profile model in users app

--- search_project/urls.py
+++ search_project/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('projects.urls')),
+    path('',include('users.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
 
--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,5 @@
 
+"""users app url handlers"""
+
+from django.contrib import admin
+from  django.urls import path 
