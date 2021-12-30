dab16fc5fdfa09e9aafec35e5e4a777975bd0243
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-12-30 17:02:26 -0600

    style: render/add  profile style

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ from  django.urls import path
 from . import views
 urlpatterns = [
     path('', views.profiles,name='profiles'),
-    # path('profiles/', views.profiles,name='profiles'),
+    path('profile/<str:pk>/', views.profile,name='my_profile'),
     # path('profiles/', views.profiles,name='profiles'),
 ]
--- users/views.py
+++ users/views.py
@@ -7,3 +7,9 @@ def profiles(request):
     users = Profile.objects.all()
     context = {'users':users}
     return render(request, 'users/profiles.html',context)
+
+
+def profile(request,pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile':profile}
+    return render(request, 'profile.html',context)
