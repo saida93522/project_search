5194c8ea2ebb0200c3cb30dd11c6359201e67d52
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-12-31 14:27:41 -0600

    refactor: displays skills,projects and social links in Profile

--- users/views.py
+++ users/views.py
@@ -11,5 +11,13 @@ def profiles(request):
 
 def profile(request,pk):
     profile = Profile.objects.get(id=pk)
-    context = {'profile':profile}
-    return render(request, 'profile.html',context)
+    topSkills = profile.skills_set.exclude(skill_description__exact="")
+    otherSkills = profile.skills_set.filter(skill_description="")
+    context = {'profile':profile, 'topSkills':topSkills,'otherSkills':otherSkills }
+    return render(request, 'users/user_profile.html',context)
+
+def skills(request,pk):
+    pass
+    # skills = Skills.objects.get(id=pk)
+    # context = {'skills':skills}
+    # return render(request, 'profile.html',context)
