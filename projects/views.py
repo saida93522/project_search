015ba698be338ce830f7123cac9f87e729676e6c
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-01-02 14:34:35 -0600

    refactor:set the currently logged in user to their project

--- projects/views.py
+++ projects/views.py
@@ -22,11 +22,14 @@ def single_project(request,pk):
 
 @login_required(login_url='login')
 def create_project(request):
+    profile = request.user.profile
     form = ProjectForm() #extensiate from projectForm
     if request.method == 'POST':
         form = ProjectForm(request.POST,request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile #set the currently logged in user to this project
+            project.save()
             return redirect('projects')
     
     context = {'form':form}
@@ -34,7 +37,8 @@ def create_project(request):
 
 @login_required(login_url='login')
 def edit_project(request,pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project) 
     if request.method == 'POST':
         form = ProjectForm(request.POST,request.FILES, instance=project)
@@ -47,7 +51,8 @@ def edit_project(request,pk):
 
 @login_required(login_url='login')
 def delete_project(request,pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
