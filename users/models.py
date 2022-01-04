192b13a580f8c277b4cd29b1b54ee5d3b99e703c
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-01-04 14:31:26 -0600

    feat: create new model which will give users the ability to message each other

--- users/models.py
+++ users/models.py
@@ -36,6 +36,7 @@ class Skills(models.Model):
     owner = models.ForeignKey('Profile', on_delete=models.CASCADE, null=True,blank=True)
     skill_name = models.CharField(max_length=200,blank=True,null=True)
     skill_description = models.TextField(blank=True,null=True)
+    
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,editable=False)
 
@@ -49,6 +50,27 @@ class Skills(models.Model):
     
 
 class Message(models.Model):
-    pass
+    #  ability to message each other
+    sender = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name="messages") #To access the profiles messages instead of doing something like profile.message_set()
+    name = models.CharField(max_length=200, blank=True,null=True)
+    email = models.EmailField(max_length=200, blank=True,null=True)
+    subject = models.CharField(max_length=200, blank=True,null=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,editable=False)
+    
+    objects = models.Manager() # provides interface between db query operations and the django model.
 
+    class Meta:
+        """Ensure all unread messages are on the top."""
+        ordering = ['is_read','-created'] #all false value will be on top
+    
+    def __str__(self):
+        return str(self.subject)
+
+   
+        
+    
  
