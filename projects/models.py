d13c95a320ec4aa8760bc66030764aa5caa07955
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-03-31 11:47:27 -0500

    test:test vote calc

--- projects/models.py
+++ projects/models.py
@@ -13,12 +13,9 @@ class Project(models.Model):
     tags = models.ManyToManyField('Tag',blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
-
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,editable=False) 
-
     objects = models.Manager() # provides interface between db query operations and the django model.
-
 
     def __str__(self):
         return self.title
@@ -45,7 +42,6 @@ class Project(models.Model):
         self.vote_ratio = ratio
         self.save()
 
-
 class Review(models.Model):
     VOTE_TYPE = (
         ('up','Up Vote'),
@@ -55,12 +51,9 @@ class Review(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
-    
-    objects = models.Manager() # provides interface between db query operations and the django model.
-
-
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,editable=False)
+    objects = models.Manager() # provides interface between db query operations and the django model.
 
     class Meta:
         """ Ensure that a user can only leave one review per project. 
@@ -73,11 +66,9 @@ class Review(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
-    
-    objects = models.Manager() # provides interface between db query operations and the django model.
-
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,editable=False)
+    objects = models.Manager() # provides interface between db query operations and the django model.
 
     def __str__(self):
         return self.name
--- projects/tests/test_model.py
+++ projects/tests/test_model.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 from django.test import TestCase
+import unittest
+from unittest.mock import patch
+
 # from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 
 from projects.models import Project, Review, Tag
+
+from users.models import Profile
+
 
 class TestProject(TestCase):
     """ Tests project model has expected fields and corresponding table columns exists.
@@ -12,20 +18,29 @@ class TestProject(TestCase):
     
     @classmethod
     def setUpTestData(cls):
-        cls.owner = Project.objects.create(
+        # USER
+        cls.user1 = User(username='admin1',email='admin@fe2.com')
+        cls.user1.is_superuser = True
+        cls.user1.is_staff = True
+        cls.user1.set_password('adminpass')
+        cls.user1.save()
+
+        #Profile
+        # cls.profile = Profile.objects.create(
+        #     user=cls.user1,
+        #     email=cls.user1.email,
+        #     )
+
+        cls.project = Project.objects.create(
         title="project1",
         description="a project that reads from a script.",
         demo_link="https://somelink.com",
         source_link="",
         vote_total=3,
         vote_ratio=4)
-        
+         
     def setUp(self):
-       user1 = User(username='admin1',email='admin@fe2.com')
-       user1.is_superuser = True
-       user1.is_staff = True
-       user1.set_password('adminpass')
-       user1.save()
+       self.client.login(username=self.user1.username,password='adminpass') 
 
     def test_user_exists(self):
         user_count = User.objects.all().count()
@@ -33,25 +48,49 @@ class TestProject(TestCase):
         self.assertNotEqual(user_count,0)
 
     def test_project_correct_data(self):
-        self.assertIsInstance(self.owner.title, str)
-        self.assertIsInstance(self.owner.description, str)
-        self.assertNotIsInstance(self.owner.vote_ratio, str)
-        self.assertIsInstance(self.owner.vote_ratio, int)
+        self.assertIsInstance(self.project.title, str)
+        self.assertIsInstance(self.project.description, str)
+        self.assertNotIsInstance(self.project.vote_ratio, str)
+        self.assertIsInstance(self.project.vote_ratio, int)
         
     def test_project_has_timestamps(self):
-        self.assertIsInstance(self.owner.created, datetime)
+        self.assertIsInstance(self.project.created, datetime)
 
     def test_string_representation(self):
-        self.assertEqual(self.owner.__str__(),self.owner.title)
+        self.assertEqual(self.project.__str__(),self.project.title)
 
     def test_project_can_have_multiple_tags(self):
         tag_names = ['django','python','html'] 
         model_tags = [Tag.objects.create() for _ in tag_names]
         for tag in model_tags:
-            self.owner.tags.add(tag) 
-        self.assertEqual(len(model_tags), self.owner.tags.count())
+            self.project.tags.add(tag) 
+        self.assertEqual(len(model_tags), self.project.tags.count())
 
+    def test_review_query(self):
+        Review.objects.create(value='Up Vote',project=self.project)
+        rev = Review.objects.get(value='Up Vote')
+        self.assertEqual(rev.value, 'Up Vote')
 
+    def test_vote_counts_calc(self):
+        Review.objects.create(value='Up Vote',project=self.project)
+
+        up_vote = Review.objects.filter(value='Up Vote').count()
+        total_votes = Review.objects.all().count()
+
+        ratio = (up_vote / total_votes) * 100
+        self.project.vote_total = total_votes
+        self.project.vote_ratio = ratio
+        self.project.save()
+        self.assertEqual(up_vote,1)        
+        self.assertEqual(total_votes,1)        
+        self.assertEqual(ratio,100.0)
+        self.assertEqual(self.project.vote_total,total_votes)        
+        self.assertEqual(self.project.vote_ratio,ratio)
+      
+
+        
+   
+             
 
 class TestReview(TestCase):
     pass
